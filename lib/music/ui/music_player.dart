ebe7bdff573cb96b09de23c75d2c8f657d543325
AK47-23 <anurudhmanosh123@gmail.com>
2023-06-26 14:52:45 +0530

    added artist view

--- lib/music/ui/music_player.dart
+++ lib/music/ui/music_player.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:music_app/music/model/track_model.dart';
 import 'package:music_app/music/provider/music_provider.dart';
@@ -31,8 +30,8 @@ class _MusicPlayerState extends State<MusicPlayer> {
   @override
   void initState() {
     super.initState();
+    init = true;
     assetsAudioPlayer = AssetsAudioPlayer.newPlayer();
-    trackModel = context.read<MusicProvider>().trackModel;
 
     VolumeController().listener((volume) {
       setState(() => setVolumeValue = volume);
@@ -44,32 +43,31 @@ class _MusicPlayerState extends State<MusicPlayer> {
   }
 
   initialise() async {
-    if (trackModel.albumName != '') {
-      try {
-        await assetsAudioPlayer.open(
-            Audio.liveStream(trackModel.previewUrl!,
-                metas: Metas(
-                  image: MetasImage.network(
-                      Common.returnAlbumImgUrl(trackModel.albumId!)),
-                  title: trackModel.name,
-                  artist: trackModel.artistName,
-                  album: trackModel.albumName,
-                )),
-            autoStart: true,
-            headPhoneStrategy: HeadPhoneStrategy.pauseOnUnplugPlayOnPlug,
-            audioFocusStrategy:
-                const AudioFocusStrategy.request(resumeAfterInterruption: true),
-            showNotification: true,
-            notificationSettings: const NotificationSettings(
-              seekBarEnabled: true,
-              playPauseEnabled: true,
-              nextEnabled: true,
-              prevEnabled: true,
-              stopEnabled: true,
-            ));
-      } catch (e) {
-        log(e.toString());
-      }
+    trackModel = context.read<MusicProvider>().trackModel;
+    try {
+      await assetsAudioPlayer.open(
+          Audio.liveStream(trackModel.previewUrl!,
+              metas: Metas(
+                image: MetasImage.network(
+                    Common.returnAlbumImgUrl(trackModel.albumId!)),
+                title: trackModel.name,
+                artist: trackModel.artistName,
+                album: trackModel.albumName,
+              )),
+          autoStart: true,
+          headPhoneStrategy: HeadPhoneStrategy.pauseOnUnplugPlayOnPlug,
+          audioFocusStrategy:
+              const AudioFocusStrategy.request(resumeAfterInterruption: true),
+          showNotification: true,
+          notificationSettings: const NotificationSettings(
+            seekBarEnabled: true,
+            playPauseEnabled: true,
+            nextEnabled: true,
+            prevEnabled: true,
+            stopEnabled: true,
+          ));
+    } catch (e) {
+      log(e.toString());
     }
   }
 
@@ -84,130 +82,160 @@ class _MusicPlayerState extends State<MusicPlayer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       appBar: Common().makeAppbar('NOW PLAYING'),
       body: context.watch<MusicProvider>().isLoading
           ? const Center(
               child: CircularProgressIndicator(),
             )
-          : Column(
-              children: [
-                Consumer<MusicProvider>(builder: (context, value, widget) {
-                  TrackModel trackModel = value.trackModel;
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(height: SizeConfig.screenHeight * .05),
-                      Center(
-                        child: trackModel.albumId != ''
-                            ? Common().makeImageResoure(
-                                trackModel.albumId!, "albums", .3, .3)
-                            : Container(
-                                width: 65.sp,
-                                height: 65.sp,
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).primaryColor,
-                                  borderRadius: BorderRadius.circular(140),
-                                ),
-                                child: Center(
-                                  child: Icon(
-                                    Icons.music_note,
-                                    size: 50.sp,
-                                    color: Colors.white,
+          : SingleChildScrollView(
+              child: Column(
+                children: [
+                  Consumer<MusicProvider>(builder: (context, value, widget) as{
+                    TrackModel trackModel = value.trackModel;
+                    if (init) {
+                      initialise();
+                      init = false;
+                      log("message");
+                    }
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: SizeConfig.screenHeight * .05),
+                        Center(
+                          child: trackModel.albumId != ''
+                              ? Common().makeImageResoure(
+                                  trackModel.albumId!, "albums", .35, .3)
+                              : Container(
+                                  width: 65.sp,
+                                  height: 65.sp,
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(context).primaryColor,
+                                    borderRadius: BorderRadius.circular(140),
                                   ),
-                                ),
-                              ),
-                      ),
-                      SizedBox(height: SizeConfig.screenHeight * .05),
-                      Text(trackModel.name!, style: titleText1),
-                      SizedBox(height: SizeConfig.screenHeight * .01),
-                      InkWell(
-                          onTap: () {
-                            //TODO
-                          },
-                          child:
-                              Text(trackModel.artistName!, style: subTitle1)),
-                      SizedBox(height: SizeConfig.screenHeight * .03),
-                      assetsAudioPlayer.builderRealtimePlayingInfos(
-                          builder: (context, RealtimePlayingInfos? infos) {
-                        if (infos == null) {
-                          return const SizedBox();
-                        }
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                          child: Column(
-                            children: [
-                              ProgressBar(
-                                progress: infos.currentPosition,
-                                total: const Duration(seconds: 29),
-                                onSeek: (Duration duration) {
-                                  log('User selected a new time: $duration');
-                                  assetsAudioPlayer.seek(duration);
-                                },
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // assetsAudioPlayer.playOrPause();
-                                  infos.isPlaying
-                                      ? assetsAudioPlayer.pause()
-                                      : assetsAudioPlayer.play();
-                                },
-                                style: ElevatedButton.styleFrom(
-                                    backgroundColor:
-                                        Theme.of(context).primaryColor,
-                                    minimumSize: Size(35.sp, 35.sp),
-                                    shape: const CircleBorder()),
-                                child: Icon(
-                                  infos.isPlaying
-                                      ? Icons.pause
-                                      : Icons.play_arrow,
-                                  color: Colors.white,
-                                  size: 30.sp,
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      }),
-                      PlayerBuilder.volume(
-                          player: assetsAudioPlayer,
-                          builder: (context, volume) {
-                            return Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.volume_down,
-                                    color: Colors.grey[400],
-                                    size: 20,
-                                  ),
-                                  Expanded(
-                                    child: Slider(
-                                      value: setVolumeValue,
-                                      onChanged: (double value) {
-                                        setState(() {
-                                          setVolumeValue = value;
-                                          VolumeController()
-                                              .setVolume(setVolumeValue);
-                                        });
-                                      },
-                                      inactiveColor: Colors.grey[800],
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.music_note,
+                                      size: 50.sp,
+                                      color: Colors.white,
                                     ),
                                   ),
-                                  Icon(
-                                    Icons.volume_up,
-                                    color: Colors.grey[400],
-                                    size: 20,
+                                ),
+                        ),
+                        SizedBox(height: SizeConfig.screenHeight * .05),
+                        Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              trackModel.name!,
+                              style: titleText1,
+                              overflow: TextOverflow.ellipsis,
+                            )),
+                        SizedBox(height: SizeConfig.screenHeight * .01),
+                        InkWell(
+                            onTap: () {
+                              //TODO
+                            },
+                            child:
+                                Text(trackModel.artistName!, style: subTitle1)),
+                        SizedBox(height: SizeConfig.screenHeight * .03),
+                        Container(
+                          height: SizeConfig.screenHeight * .35,
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 30, horizontal: 20),
+                          decoration: const BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(25),
+                                  topRight: Radius.circular(25)),
+                              color: Colors.white60),
+                          child: Column(
+                            children: [
+                              assetsAudioPlayer.builderRealtimePlayingInfos(
+                                  builder:
+                                      (context, RealtimePlayingInfos? infos) {
+                                if (infos == null) {
+                                  return const SizedBox();
+                                }
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 12.0),
+                                  child: Column(
+                                    children: [
+                                      ProgressBar(
+                                        progress: infos.currentPosition,
+                                        total: const Duration(seconds: 29),
+                                        onSeek: (Duration duration) {
+                                          assetsAudioPlayer.seek(duration);
+                                        },
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          assetsAudioPlayer.playOrPause();
+                                          // infos.isPlaying
+                                          //     ? assetsAudioPlayer.pause()
+                                          //     : assetsAudioPlayer.play();
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor:
+                                                Theme.of(context).primaryColor,
+                                            minimumSize: Size(35.sp, 35.sp),
+                                            shape: const CircleBorder()),
+                                        child: Icon(
+                                          infos.isPlaying
+                                              ? Icons.pause
+                                              : Icons.play_arrow,
+                                          color: Colors.white,
+                                          size: 30.sp,
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                ],
-                              ),
-                            );
-                          }),
-                    ],
-                  );
-                }),
-              ],
+                                );
+                              }),
+                              PlayerBuilder.volume(
+                                  player: assetsAudioPlayer,
+                                  builder: (context, volume) {
+                                    return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Icon(
+                                            Icons.volume_down,
+                                            color: Colors.grey[400],
+                                            size: 20,
+                                          ),
+                                          Expanded(
+                                            child: Slider(
+                                              value: setVolumeValue,
+                                              onChanged: (double value) {
+                                                setState(() {
+                                                  setVolumeValue = value;
+                                                  VolumeController().setVolume(
+                                                      setVolumeValue);
+                                                });
+                                              },
+                                              inactiveColor: Colors.grey[800],
+                                            ),
+                                          ),
+                                          Icon(
+                                            Icons.volume_up,
+                                            color: Colors.grey[400],
+                                            size: 20,
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  }),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+                ],
+              ),
             ),
     );
   }
